85d0390eed466252fd346b5329a383b0fe99d412
Daria Kliueva <dahapost@gmail.com>
2024-09-13 17:07:17 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -27,11 +27,11 @@ function App({ store }) {
                 onClick={() => store.selectItem(item.code)}
               >
                 <div className="Item-code">{item.code}</div>
-                <div className="Item-title">{item.title}</div>
-                {item.selectionCount > 0 && (
-        <div className="Item-selectionCount">Выделяли {item.selectionCount} раз</div>
-      )}
-                <div className="Item-actions">
+                <div className="Item-title">
+                  {item.title}{' '}
+                  {item.selectedCount > 0 &&
+                    ` | Выделяли ${item.selectedCount} раз`}</div>
+                   <div className="Item-actions">
                   <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
                 </div>
               </div>
