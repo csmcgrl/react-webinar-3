096f7fa3cbed05b92e7badc78b60de22823b8709
Daria Kliueva <dahapost@gmail.com>
2024-09-19 22:17:54 +0700

    Update index.js

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -3,41 +3,31 @@ import PropTypes from 'prop-types';
 import { plural } from '../../utils';
 import './style.css';
 
-function Item(props) {
-  // Счётчик выделений
-  const [count, setCount] = useState(0);
-
+function Item({ item = {}, onClick = () => {}, name = '' }) {
   const callbacks = {
-    onClick: () => {
-      props.onSelect(props.item.code);
-      if (!props.item.selected) {
-        setCount(count + 1);
-      }
-    },
-    onDelete: e => {
+    onClick: e => {
       e.stopPropagation();
-      props.onDelete(props.item.code);
+      onClick(item.code);
     },
   };
 
   return (
-    <div
-      className={'Item' + (props.item.selected ? ' Item_selected' : '')}
-      onClick={callbacks.onClick}
-    >
-      <div className="Item-code">{props.item.code}</div>
+    <div className="Item">
       <div className="Item-title">
-        {props.item.title}{' '}
-        {count
-          ? ` | Выделяли ${count} ${plural(count, {
-              one: 'раз',
-              few: 'раза',
-              many: 'раз',
-            })}`
-          : ''}
+        <div className="Item-code">{item.code}</div>
+        <div className="Item-name">{item.title}</div>
       </div>
       <div className="Item-actions">
-        <button onClick={callbacks.onDelete}>Удалить</button>
+        <div
+          className="Item-info"
+          style={!item.count ? { justifyContent: 'end' } : { justifyContent: 'spaceBetween' }}
+        >
+          <div className="Item-price">{item.price + ' \u20bd'}</div>
+          {item.count ? <div className="Item-count">{item.count + ' шт'}</div> : ''}
+        </div>
+        <button className="Item-button" onClick={callbacks.onClick}>
+          {name}
+        </button>
       </div>
     </div>
   );
@@ -47,16 +37,11 @@ Item.propTypes = {
   item: PropTypes.shape({
     code: PropTypes.number,
     title: PropTypes.string,
-    selected: PropTypes.bool,
+    price: PropTypes.number,
     count: PropTypes.number,
   }).isRequired,
-  onDelete: PropTypes.func,
-  onSelect: PropTypes.func,
-};
-
-Item.defaultProps = {
-  onDelete: () => {},
-  onSelect: () => {},
+  onClick: PropTypes.func,
+  name: PropTypes.string,
 };
 
 export default React.memo(Item);
