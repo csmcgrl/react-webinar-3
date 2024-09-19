b333ee61f92bb1dbf3848477bed9d51dead8e9a2
Daria Kliueva <dahapost@gmail.com>
2024-09-19 22:15:21 +0700

    Update index.js

--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -1,21 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './style.css';
+import { plural } from '../../utils';
 
-function Controls({ onAdd }) {
+function Controls({ onClickModal = () => {}, name = '', totalCount = -1, totalCost = 0 }) {
   return (
     <div className="Controls">
-      <button onClick={() => onAdd()}>Добавить</button>
+      {totalCount == -1 ? (
+        ''
+      ) : (
+        <div className="Controls-info">
+          В корзине:{' '}
+          <span className="Controls-info__mark">
+            {totalCount === 0
+              ? 'пусто'
+              : `${totalCount} ${plural(totalCount, {
+                  one: 'товар',
+                  few: 'товара',
+                  many: 'товаров',
+                })} / ${totalCost} \u20bd`}
+          </span>
+        </div>
+      )}
+
+      <button className="Controls-button" onClick={() => onClickModal()}>
+        {name}
+      </button>
     </div>
   );
 }
 
 Controls.propTypes = {
-  onAdd: PropTypes.func,
-};
-
-Controls.defaultProps = {
-  onAdd: () => {},
+  onClickModal: PropTypes.func,
+  name: PropTypes.string,
+  totalCount: PropTypes.number,
+  totalCost: PropTypes.number,
 };
 
 export default React.memo(Controls);
